341bf4dea616af7f6f4a8bf12f6ff968ea7a7c8f
Animish2002 <animishchopade123@gmail.com>
2024-08-07 10:58:52 +0530

    first setup complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <h4 className="text-3xl">WICKWEAR</h4>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
